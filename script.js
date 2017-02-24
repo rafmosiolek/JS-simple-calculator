92798907a4ac7afca693a35e9e3d8dcedd9a6ca4
RafM <rkjmosiolek@gmail.com>
2017-02-24 11:30:17 +0100

    decimal point function added

--- script.js
+++ script.js
@@ -7,17 +7,26 @@ Operation = 0;      // Records code for eg * / etc.
 MAXLENGTH = 30;     // maximum number of digits before decimal!
 
 
-function AddDigit(dig)          //ADD A DIGIT TO DISPLAY (kept as 'Current')
- {if (Current.length > MAXLENGTH)
-       { Current = "Aargh! Too long"; //limit length
-       } else
-       { if (    (eval(Current) === 0)
-              && (Current.indexOf(".") === -1)
-            )
-           { Current = dig;
-           } else
-           { Current = Current + dig;
-           };
-    }; };
-   document.Calculator.Display.value = Current;
- }
+function AddDigit(dig) {        // ADD A DIGIT TO DISPLAY (kept as "Current")
+    if (Current.length > MAXLENGTH) {
+        Current = "Too long!";      // limit length
+    } else {
+        if (eval(Current) === 0) && (Current.indexOf(".") === -1) {
+            Current = dig;
+        } else {
+            Current = Current + dig;
+        };
+    };
+    document.Calculator.Display.value = Current;
+}
+
+
+function Dot () {
+    if (Current.length === 0) {     // no leading ".", use "0."
+        Current = "0.";
+    } else {
+       if (Current.indexOf(".") === -1)
+        { Current = Current + ".";};
+    };
+    document.Calculator.Display.value = Current;
+}
