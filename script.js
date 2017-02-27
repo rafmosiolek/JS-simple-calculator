d2816470b9735520d004a7f911dacea00fa4370d
RafM <rkjmosiolek@gmail.com>
2017-02-27 18:56:33 +0100

    function PlusMinus() fixed

--- script.js
+++ script.js
@@ -10,37 +10,38 @@ MAXLENGTH = 30;     // maximum number of digits before decimal!
 
 
 function AddDigit(dig)          //ADD A DIGIT TO DISPLAY (keep as 'Current')
- { if (Current.indexOf("!") == -1)  //if not already an error
-    { if (    (eval(Current) == 0)
-              && (Current.indexOf(".") == -1)
-         ) { Current = dig;
-           } else
-           { Current = Current + dig;
-           };
+ { if (Current.indexOf("!") === -1)  //if not already an error
+ { if (    (eval(Current) === 0)
+  && (Current.indexOf(".") == -1)
+  ) { Current = dig;
+ } else
+ { Current = Current + dig;
+ };
       Current = Current.toLowerCase(); //FORCE LOWER CASE
-    } else
+  } else
     { Current = "Hint! Press 'AC'";  //Help out, if error present.
-    };
-   if (Current.indexOf("e0") != -1)
-     { var epos = Current.indexOf("e");
-       Current = Current.substring(0,epos+1) + Current.substring(epos+2);
-     };
-  if (Current.length > MAXLENGTH)
+};
+if (Current.indexOf("e0") != -1)
+   { var epos = Current.indexOf("e");
+Current = Current.substring(0,epos+1) + Current.substring(epos+2);
+};
+if (Current.length > MAXLENGTH)
      { Current = "Aargh! Too long"; //don't allow over MAXLENGTH digits before "." ???
-     };
-   document.Calculator.Display.value = Current;
- }
+};
+document.Calculator.Display.value = Current;
+}
 
 
-function Dot () {
+function Dot () {       // put in "." if appropriate
     if (Current.length === 0) {     // no leading ".", use "0."
         Current = "0.";
-    } else {
-       if (Current.indexOf(".") === -1)
-        { Current = Current + ".";};
-    };
-    document.Calculator.Display.value = Current;
+} else {
+ if (Current.indexOf(".") === -1)
+    { Current = Current + ".";};
+};
+document.Calculator.Display.value = Current;
 }
+
 
 function DoExponent() {     // DoExponent function handles the exponentation 10^(an integer)
     if (Current.indexOf("e") === -1) {
@@ -50,24 +51,28 @@ function DoExponent() {     // DoExponent function handles the exponentation 10^
 
 }
 
-function PlusMinus () {
-    if (Current.indexOf("e") != -1) {       // if there's an exponent:
-        var epos = Current.indexOf("e-");   
-        if (epos != -1) {
-            Current = Current.substring(0,1+epos) + Current.substring(2+epos);  // clip -ve exp
-        } else {
-            epos = Current.indexOf("e");
-            Current = Current.substring(0,1+epos) + "-" + Current.substring(1+epos);    // insert
-        }
-    } else {                                // there's NO exponent:
-        if (Current.indexOf("-") === 0) {
-            Current = Current.substring(1);
-        } else {
-            Current = "-" + Current;
-        }
-    }
-    document.Calculator.Display.value = Current;
+function PlusMinus()
+{
+  if  (Current.indexOf("e") != -1)
+    { var epos = Current.indexOf("e-");
+if (epos != -1)
+         { Current = Current.substring(0,1+epos) + Current.substring(2+epos); //clip out -ve exponent 
+         } else
+         { epos = Current.indexOf("e");
+           Current = Current.substring(0,1+epos) + "-" + Current.substring(1+epos); //insert -ve exponent
+       };
+   } else
+   {  if ( Current.indexOf("-") === 0 )
+   { Current = Current.substring(1);
+   } else
+   { Current = "-" + Current;
 };
+if (    (eval(Current) === 0)
+    && (Current.indexOf(".") === -1 )
+    ) { Current = "0"; };
+};
+document.Calculator.Display.value = Current;
+}
 
 function Clear() {          // clear entry [C]
     Current = "0";
