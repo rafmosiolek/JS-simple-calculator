a7be5302af2eb99824564c98a1e88472f01fe9f7
RafM <rkjmosiolek@gmail.com>
2017-03-13 00:14:03 +0100

    minor fixes added, function still does not work

--- script.js
+++ script.js
@@ -8,49 +8,49 @@ MAXLENGTH = 30;     // maximum number of digits before decimal!
 
 
 
-function AddDigit(dig)          //ADD A DIGIT TO DISPLAY (keep as 'Current')
+function addDigit(dig)          //ADD A DIGIT TO DISPLAY (keep as 'Current')
  { if (Current.indexOf("!") === -1)  //if not already an error
  { if (    (eval(Current) === 0)
   && (Current.indexOf(".") == -1)
   ) { Current = dig;
  } else
  { Current = Current + dig;
- };
+ }
       Current = Current.toLowerCase(); //FORCE LOWER CASE
   } else
     { Current = "Hint! Press 'AC'";  //Help out, if error present.
-};
+}
 if (Current.indexOf("e0") != -1)
  { var epos = Current.indexOf("e");
 Current = Current.substring(0,epos+1) + Current.substring(epos+2);
-};
+}
 if (Current.length > MAXLENGTH)
      { Current = "Aargh! Too long"; //don't allow over MAXLENGTH digits before "." ???
-};
+}
 document.Calculator.Display.value = Current;
 }
 
 
-function Dot () {       // put in "." if appropriate
+function dot () {       // put in "." if appropriate
     if (Current.length === 0) {     // no leading ".", use "0."
         Current = "0.";
 } else {
    if (Current.indexOf(".") === -1)
-    { Current = Current + ".";};
-};
+    { Current = Current + ".";}
+}
 document.Calculator.Display.value = Current;
 }
 
 
-function DoExponent() {     // DoExponent function handles the exponentation 10^(an integer)
+function doExponent() {     // DoExponent function handles the exponentation 10^(an integer)
     if (Current.indexOf("e") === -1) {
         Current = Current + "e0";
         document.Calculator.Display.value = Current;
-    };
+    }
 
 }
 
-function PlusMinus()
+function plusMinus()
 {
   if  (Current.indexOf("e") != -1)
     { var epos = Current.indexOf("e-");
@@ -65,27 +65,27 @@ if (epos != -1)
    { Current = Current.substring(1);
    } else
    { Current = "-" + Current;
-};
+}
 if (    (eval(Current) === 0)
     && (Current.indexOf(".") === -1 )
     ) { Current = "0"; };
-};
+}
 document.Calculator.Display.value = Current;
 }
 
-function Clear() {          // clear entry [C]
+function clear() {          // clear entry [C]
     Current = "0";
     document.Calculator.Display.value = Current;
 }
 
-function AllClear() {       // Clear all entries [AC]
+function allClear() {       // Clear all entries [AC]
     Current = "0";
     Operation = 0;          // clear operation
     Memory = "0";           // clear memory
     document.Calculator.Display.value = Current;
 }
 
-function Operate(op) {          // store operation, e.g. + * /
+function operate(op) {          // store operation, e.g. + * /
     if (Operation != 0) {Calculate();} // press "=" if pending operation!
 
     if (op.indexOf("*") > -1) {Operation = 1;};     //codes for *
@@ -98,7 +98,7 @@ function Operate(op) {          // store operation, e.g. + * /
     document.Calculator.Display.value = Current;
 }
 
-function Calculate()            //PERFORM CALCULATION (= button)
+function calculate()            //PERFORM CALCULATION (= button)
 { 
   if (Operation === 1) { Current = eval(Memory) * eval(Current); };
   if (Operation === 2)
@@ -115,20 +115,20 @@ if (Operation === 4) { Current = eval(Memory) - eval(Current); };
   Current = Current + "";       //FORCE A STRING!
   if (Current.indexOf("Infinity") != -1)        //eg "1e320" * 1
     { Current = "Value too big!";
-};
+}
   if (Current.indexOf("NaN") != -1)        //eg "1e320" / "1e320"
     { Current = "I don't understand";
-};
+}
 document.Calculator.Display.value = Current;
   // NOTE: if no operation, nothing changes, Current is left the same!
 }
 
 
-function FixCurrent() {         // dealing with stuff typed into the display area
+function fixCurrent() {         // dealing with stuff typed into the display area
     Current = document.Calculator.Display.value;
     Current = "" + parseFloat(Current);
     if (Current.indexOf("NaN") != -1) {
         Current = "I don't understand!";
-    };
+    }
     document.Calculator.Display.value = Current;
 }
